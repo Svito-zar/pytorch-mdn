318162319e845a10c3c62702187d13751b4fff10
Svito-zar <taras.svitozar@gmail.com>
2019-08-13 13:42:41 +0200

    Modified MDN model

--- mdn.py
+++ mdn.py
@@ -9,8 +9,7 @@ from torch.autograd import Variable
 from torch.distributions import Categorical
 import math
 
-
-ONEOVERSQRT2PI = 1.0 / math.sqrt(2*math.pi)
+torch.set_default_tensor_type('torch.FloatTensor')
 
 class MDN(nn.Module):
     """A mixture density network layer
@@ -39,24 +38,22 @@ class MDN(nn.Module):
         self.in_features = in_features
         self.out_features = out_features
         self.num_gaussians = num_gaussians
-        self.pi = nn.Sequential(
-            nn.Linear(in_features, num_gaussians),
-            nn.Softmax(dim=1)
-        )
-        self.sigma = nn.Linear(in_features, out_features*num_gaussians)
+        self.pi_raw = nn.Linear(in_features, num_gaussians)
+        self.log_sigma = nn.Linear(in_features, out_features*num_gaussians)
         self.mu = nn.Linear(in_features, out_features*num_gaussians)
 
     def forward(self, minibatch):
-        pi = self.pi(minibatch)
-        sigma = torch.exp(self.sigma(minibatch))
+        pi_raw = self.pi_raw(minibatch).reshape(minibatch.shape[0],-1)
+        pi = nn.Softmax(dim=-1)(pi_raw)
+        sigma = torch.exp(self.log_sigma(minibatch)) + 1e-10
         sigma = sigma.view(-1, self.num_gaussians, self.out_features)
         mu = self.mu(minibatch)
         mu = mu.view(-1, self.num_gaussians, self.out_features)
         return pi, sigma, mu
 
 
-def gaussian_probability(sigma, mu, target):
-    """Returns the probability of `data` given MoG parameters `sigma` and `mu`.
+def log_gaussian_probability(sigma, mu, pi, target):
+    """Returns the log-probability of `data` given MoG parameters `sigma` and `mu` and 'pi'.
     
     Arguments:
         sigma (BxGxO): The standard deviation of the Gaussians. B is the batch
@@ -64,16 +61,26 @@ def gaussian_probability(sigma, mu, target):
             dimensions per Gaussian.
         mu (BxGxO): The means of the Gaussians. B is the batch size, G is the
             number of Gaussians, and O is the number of dimensions per Gaussian.
-        data (BxI): A batch of data. B is the batch size and I is the number of
+        target (BxI): A batch of data. B is the batch size and I is the number of
             input dimensions.
 
     Returns:
-        probabilities (BxG): The probability of each point in the probability
+        average of log_probabilities (BxG): Average value of the log-probability of each point in the probability
             of the distribution in the corresponding sigma/mu index.
     """
-    target = target.unsqueeze(1).expand_as(sigma)
-    ret = ONEOVERSQRT2PI * torch.exp(-0.5 * ((data - mu) / sigma)**2) / sigma
-    return torch.prod(ret, 2)
+
+
+    target = target.unsqueeze(1).expand_as(sigma).float()
+
+    out_shape = sigma.shape[-1]
+    const = .5 * out_shape * math.log(2 * math.pi)
+
+    exponent = torch.log(pi) - const \
+               - torch.sum(((target - mu)/sigma)**2 / 2 + torch.log(sigma) ,[2])
+
+    log_gauss = torch.logsumexp(exponent, dim=1)
+    res = - torch.mean(log_gauss)
+    return res
 
 
 def mdn_loss(pi, sigma, mu, target):
@@ -82,9 +89,8 @@ def mdn_loss(pi, sigma, mu, target):
     The loss is the negative log likelihood of the data given the MoG
     parameters.
     """
-    prob = pi * gaussian_probability(sigma, mu, target)
-    nll = -torch.log(torch.sum(prob, dim=1))
-    return torch.mean(nll)
+    loss =  log_gaussian_probability(sigma, mu, pi, target)
+    return loss
 
 
 def sample(pi, sigma, mu):
